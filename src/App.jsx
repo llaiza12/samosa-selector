448052147a513edf4dbede1113890a8d30f5c172
Laiza <llaizanicole@gmail.com>
2025-04-19 22:51:44 -0400

    Upgrade functionality for app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,24 @@ const App = () => {
   const [count, setCount] = useState(0); // keep track of number of total clicks
   const [multiplier, setMultiplier] = useState(1); // keep track of current number of samosas per click
   const updateCount = () => setCount(count + multiplier); // sets value of the state var count to its current value + multiplier
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+      setCount(count - 10);
+    }
+  }
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+      setCount(count - 100);
+    }
+  }
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier(multiplier * 10);
+      setCount(count - 1000);
+    }
+  }
 
   // describes the structure of the UI
   return (
@@ -18,17 +36,17 @@ const App = () => {
       <div className="upgrade">
         <h3>Double Stuffed 👯‍♀️</h3>
         <p>2x per click</p>
-        <button>10 samosas</button>
+        <button onClick={buyDoubleStuffed}>10 samosas</button>
       </div>
       <div className="upgrade">
         <h3>Party Pack 🎉</h3>
         <p>5x per click</p>
-        <button>	100 samosas</button>
+        <button onClick={buyPartyPack}>	100 samosas</button>
       </div>
       <div className="upgrade">
         <h3>Full Feast 👩🏽‍🍳</h3>
         <p>10x per click</p>
-        <button>1000 samosas</button>
+        <button onClick={buyFullFeast}>1000 samosas</button>
       </div>
       </div>
     </div>
